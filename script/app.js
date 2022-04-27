3735baa531dd3294528c3a6b3171dfe6b5611b59
library QZ <libraryqz@mail.ru>
2022-04-27 19:42:12 +0300

    add.js

---
+++ script/app.js
@@ -0,0 +1,41 @@
+/*console.log('hello world');
+alert('hello world');
+confirm('admin');
+prompt('hello world');
+//number
+//string
+//boolean
+//bigint l
+//symbol
+//objekt
+//function
+//null
+//== ===>=<=><
+var a = 1;
+let b = "any";
+const c = true;
+let a = conf;
+conf=confirm('any question')
+console.log(conf);
+console.log(null = 0);
+console.log(typeof b);
+console.log('num');
+let res = 1 * 'v2';
+let str1 = 'dima';
+str2 = '1';
+console.log('$(str1) $(str2)');
+//+ - /* *
+left num = 2;
+let com = confirm('any')
+//появляется confirm
+//появляется окно  ввода пароля
+let com = confirm('admin')
+if(typeof conf === 'boolean') {
+ alert('yes')
+} else {
+ alert('no')
+} */
+console.log(1 | underfined | 1 | null) && &&
+
+
+
